167bf3b6a277b13cca2dfba905ead3dc541e6595
Jussi Tiira <jussi@j24.fi>
2018-05-16 00:57:57 +0300

    support other location formats

--- fmi2mqtt/__main__.py
+++ fmi2mqtt/__main__.py
@@ -3,12 +3,12 @@
 """
 from paho.mqtt import publish
 import fmi
-from config import config
+from config import config, get_place
 
 if __name__ == '__main__':
     topicfmt = 'fmi/{place}/{param}'
     weather = fmi.get_weather_simple(**config['query']).iloc[-1]
     for param, value in weather.iteritems():
-        topic = topicfmt.format(place=config['query']['place'], param=param)
+        topic = topicfmt.format(place=get_place(), param=param)
         publish.single('fmi/{place}/{param}', value,
                        hostname=config['mqtt']['host'], auth=config['mqtt'])
--- fmi2mqtt/config.py
+++ fmi2mqtt/config.py
@@ -7,3 +7,9 @@ import configparser
 global_file = '/etc/fmi2mqtt.conf'
 config = configparser.ConfigParser()
 config.read(global_file)
+
+
+def get_place(config=config):
+    keys = [k for k in config['query'].keys()]
+    keys.remove('parameters')
+    return config['query'][keys[0]]
