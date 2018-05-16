44c410bc4f1691c54fd0b04c0fa644e73481c0ca
Jussi Tiira <jussi@j24.fi>
2018-05-16 22:59:27 +0300

    main function with graceful killer

--- fmi2mqtt/__main__.py
+++ fmi2mqtt/__main__.py
@@ -1,14 +1,27 @@
 # coding: utf-8
 """
 """
+import time
 from paho.mqtt import publish
 import fmi
 from config import config, get_place
+from j24.server import GracefulKiller
+
+
+def main():
+    killer = GracefulKiller()
+    while True:
+        topicfmt = 'fmi/{place}/{param}'
+        weather = fmi.get_weather_simple(**config['query']).iloc[-1]
+        for param, value in weather.iteritems():
+            topic = topicfmt.format(place=get_place(), param=param)
+            publish.single(topic, value, hostname=config['mqtt']['host'],
+                           auth=config['mqtt'])
+        if killer.kill_now:
+            break
+        time.sleep(300)
+    print('Stopped gracefully.')
+
 
 if __name__ == '__main__':
-    topicfmt = 'fmi/{place}/{param}'
-    weather = fmi.get_weather_simple(**config['query']).iloc[-1]
-    for param, value in weather.iteritems():
-        topic = topicfmt.format(place=get_place(), param=param)
-        publish.single('fmi/{place}/{param}', value,
-                       hostname=config['mqtt']['host'], auth=config['mqtt'])
+    main()
